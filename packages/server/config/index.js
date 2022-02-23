75e7f2ef8ca89aa449d0cd54ab94dfdab948a766
melanker <alonm@traiana.com>
2022-02-23 11:34:23 +0200

    all files

--- packages/server/config/index.js
+++ packages/server/config/index.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development'
 
 const baseConfig = {
   isDev: env === 'development',
-  port: process.env.PORT || 5000,
+  port: 5000,
   secrets: {
     jwt: 'secret should be environment variable'
   },
